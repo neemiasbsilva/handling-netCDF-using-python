171d219179fa1b3bcb0f37d3c007636b15f5e61f
neemiasbsilva <neemiasbsilva@gmail.com>
2020-06-28 15:48:26 -0400

    shell script for run_mass_downloading.py

---
+++ run_mass_downloading_script.sh
@@ -0,0 +1,6 @@
+python mass_downloading_netCDF.py \
+-url_name http://aphrodite.st.hirosaki-u.ac.jp/product/APHRO_V1808_TEMP/APHRO_MA/025deg_nc/ \
+-begin_year 1961 \
+-end_year 1969 \
+-user_name xxxxx@xxxxx.com \
+-password xxxxxxxx
